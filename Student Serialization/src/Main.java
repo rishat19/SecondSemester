9b251004a291584eda6b3d10914815094fa44ece
rishat19 <57725670+rishat19@users.noreply.github.com>
2020-03-24 23:29:08 +0300

    HW 9

---
+++ Student Serialization/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Ахметханов Ринат", 902, 78.25));
+        students.add(new Student("Волкова Оливия", 902, 71.125));
+        students.add(new Student("Газизова Милана", 902, 79));
+        students.add(new Student("Ганиев Ришат", 902, 95));
+        students.add(new Student("Гилемзянов Карим", 902, 80.375));
+        students.add(new Student("Гилязов Азат", 902, 83.5));
+        students.add(new Student("Давлетшин Дамир", 902, 81.625));
+        students.add(new Student("Динер Александр", 902, 85));
+        students.add(new Student("Дьяконов Алексей", 902, 84));
+        students.add(new Student("Евсикова Ангелина", 902, 86));
+        students.add(new Student("Закиер Данияр", 902, 83.875));
+        students.add(new Student("Калимуллин Искандер", 902, 70.25));
+        students.add(new Student("Меметова Эвелина", 902, 77.75));
+        students.add(new Student("Муксинова Дана", 902, 80.375));
+        students.add(new Student("Мухина Дарья", 902, 78.25));
+        students.add(new Student("Насибуллин Эрик", 902, 75.25));
+        students.add(new Student("Нигматьянов Рифдар", 902, 82.5));
+        students.add(new Student("Садритдинов Урал", 902, 72));
+        students.add(new Student("Свидиров Кирилл", 902, 90.625));
+        students.add(new Student("Степанищева Анастасия", 902, 81.125));
+        students.add(new Student("Трофимов Даниил", 902, 86.75));
+        students.add(new Student("Тыщенко Леонид", 902, 86.75));
+        students.add(new Student("Фахрутдинов Булат", 902, 84.875));
+        students.add(new Student("Фёдорова Алина", 902, 77.375));
+        StudentSerialisation.write(students,"src\\test.txt");
+        System.out.println(StudentSerialisation.read("src\\test.txt").toString());
+    }
+}
---
+++ Student Serialization/src/StudentSerialisation.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class StudentSerialisation {
+
+    public static void write(Collection<Student> students, String path) {
+        try (OutputStream out = new FileOutputStream(path)) {
+            ByteBuffer sizeBuffer = ByteBuffer.allocate(Integer.BYTES);
+            sizeBuffer.putInt(students.size());
+            out.write(sizeBuffer.array());
+            out.flush();
+            for (Student student : students) {
+                ByteBuffer studentBuffer = ByteBuffer.allocate(Character.BYTES * student.getName().length() +
+                        2 * Integer.BYTES + Double.BYTES);
+                studentBuffer.putInt(student.getName().length());
+                for (int i = 0; i < student.getName().length(); i++) {
+                    studentBuffer.putChar(student.getName().charAt(i));
+                }
+                studentBuffer.putInt(student.getGroup());
+                studentBuffer.putDouble(student.getRating());
+                out.write(studentBuffer.array());
+                out.flush();
+                studentBuffer.clear();
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("File not found.");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Collection<Student> read(String path) {
+        Collection<Student> students = new ArrayList<>();
+        String name;
+        int group;
+        double rating;
+        try (InputStream in = new FileInputStream(path)) {
+            ByteBuffer buffer = ByteBuffer.allocate(Double.BYTES);
+            for (int i = 0; i < Integer.BYTES; i++) {
+                buffer.put((byte) in.read());
+            }
+            buffer.rewind();
+            int size = buffer.getInt();
+            buffer.clear();
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < Integer.BYTES; j++) {
+                    buffer.put((byte) in.read());
+                }
+                buffer.rewind();
+                int nameSize = buffer.getInt();
+                buffer.clear();
+                char[] studentName = new char[nameSize];
+                for (int j = 0; j < nameSize; j++) {
+                    buffer.put((byte) in.read());
+                    buffer.put((byte) in.read());
+                    buffer.rewind();
+                    studentName[j] = buffer.getChar();
+                    buffer.clear();
+                }
+                name = new String(studentName);
+                for (int j = 0; j < Integer.BYTES; j++) {
+                    buffer.put((byte) in.read());
+                }
+                buffer.rewind();
+                group = buffer.getInt();
+                buffer.clear();
+                for (int j = 0; j < Double.BYTES; j++) {
+                    buffer.put((byte) in.read());
+                }
+                buffer.rewind();
+                rating = buffer.getDouble();
+                buffer.clear();
+                students.add(new Student(name, group, rating));
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return students;
+    }
+
+}
